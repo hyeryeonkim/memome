5749137ece149ac814ab900d023a293549823815
hyeryeonkim <kim5638yw@naver.com>
2020-09-02 01:00:12 +0900

    db.sql 추가, 회원가입 시, lastPasswordUpdateDate attr 변수 생성 오류 해결

--- db.sql
+++ db.sql
@@ -14,8 +14,8 @@ CREATE TABLE `member` (
     loginPw CHAR(100) NOT NULL,
     `name` CHAR(20) NOT NULL,
     `nickname` CHAR(20) NOT NULL UNIQUE,
-    `email` CHAR(100) NOT NULL UNIQUE
-      
+    `email` CHAR(100) NOT NULL UNIQUE,
+    `cellphoneNo` CHAR(20) NOT NULL 
 );
 
 SELECT *
@@ -46,7 +46,22 @@ CREATE TABLE article (
     title CHAR(200) NOT NULL,
     `body` LONGTEXT NOT NULL,
     memberId INT(10) UNSIGNED NOT NULL,
-    boardId INT(10) UNSIGNED NOT NULL 
+    boardId INT(10) UNSIGNED NOT NULL,
+    memoId INT(10) UNSIGNED NOT NULL
+);
+
+# memo 카테고리  테이블 생성
+DROP TABLE IF EXISTS memo;
+CREATE TABLE memo (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    delDate DATETIME,
+    delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
+    displayStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
+    `name` CHAR(200) NOT NULL,
+    memberId INT(10) UNSIGNED NOT NULL,
+    boardId INT(10) UNSIGNED NOT NULL
 );
 
 INSERT INTO `board`
@@ -61,12 +76,27 @@ updateDate = NOW(),
 `code` = 'notice',
 `name` = '공지사항';
 
+
+INSERT INTO `board`
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'memome',
+`name` = '나의메모';
+
+
+INSERT INTO `board`
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'memoyou',
+`name` = '모두의메모';
+
+
 SELECT *
 FROM `member`;
 
-SELECT * 
-FROM article
-ORDER BY id DESC
+# SELECT * 
+# FROM article
+# ORDER BY id DESC
 
 
 # 부가정보테이블(임시 비밀번호 발송 여부, 개인정보 변경 외)
@@ -117,3 +147,25 @@ ALTER TABLE `file` ADD UNIQUE INDEX (`relId`, `relTypeCode`, `typeCode`, `type2C
 
 # 파일 테이블의 기존 인덱스에 유니크가 걸려 있어서 relId가 0 인 동안 충돌이 발생할 수 있다. 그래서 일반 인덱스로 바꾼다.
 ALTER TABLE `file` DROP INDEX `relId`, ADD INDEX (`relId` , `relTypeCode` , `typeCode` , `type2Code` , `fileNo`); 
+
+SELECT *
+FROM `attr`;
+
+SELECT *
+FROM `member`;
+
+SELECT *
+FROM board;
+
+SELECT *
+FROM memo;
+
+SELECT *
+FROM memo;
+
+SELECT *
+FROM article;
+
+
+TRUNCATE memo;
+TRUNCATE article;
